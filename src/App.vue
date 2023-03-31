b665e35a29beee2eb9981e0394bdc59bdca8235f
dope <mdc@kakao.com>
2023-03-31 09:33:14 +0900

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <br>
+  <a href="http://10.0.1.112:8084">WA</a>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
